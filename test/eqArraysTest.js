6be42ef01a8d77332e74db1d244bb9a18d8dc4fe
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-05 20:10:36 -0600

    mocha test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,6 +1,7 @@
+const { assert } = require('chai');
 const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
-
+/*
 const testOne = eqArrays([1, 2, 3], [1, 2, 3]) 
 const testTwo = eqArrays([1, 2, 3], [3, 2, 1]) 
 const testThree = eqArrays(["1", "2", "3"], ["1", "2", "3"]) 
@@ -10,3 +11,15 @@ assertEqual(testOne, true);
 assertEqual(testTwo, true);
 assertEqual(testThree, true);
 assertEqual(testFour, true);
+*/
+
+describe('#eqArrays', () => {
+  it("returns true when passed eqArrays([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns false when passed eqArrays([1, 2, 3], [1, 2, 3])", () => {
+    assert.strictEqual(eqArrays([1, 3], [1, 2, 3]), false);
+
+  });
+});
