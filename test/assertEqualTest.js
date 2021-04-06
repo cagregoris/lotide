561c72770a1dd4f59a9b17d92f0427ad7395e2ff
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-05 20:54:35 -0600

    refactored with mocha

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,7 @@
+const { assert } = require('chai');
 const assertEqual = require('../assertEqual');
 
-//assert equal test code
+
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("Hello", "Hello");
--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,20 @@
+const assert = require('chai').assert;
 const assertArraysEqual = require('../assertArraysEqual');
 const middle = require('../middle');
 
+/*
 console.log(assertArraysEqual(middle([1, 2, 4, 7, 8]), [6]));
 console.log(assertArraysEqual(middle([1, 2, 4, 6, 7, 8]), [4, 6]));
 console.log(assertArraysEqual(middle([1, 3, 4, 7, 8]), [4]));
+*/
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2])
+  })
+
+  it("returns [4, 6] for [1, 2, 4, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 4, 6, 7, 8]), [4, 6])
+  })
+
+});
