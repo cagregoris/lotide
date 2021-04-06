39bce6636b7b88c49c2b74956b0fec50f9f730be
Carolyn Gregoris <carolyn.a.scobie@gmail.com>
2021-04-05 19:37:14 -0600

    refactored with mocha

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,5 +1,19 @@
+const { assert } = require('chai');
 const assertArraysEqual = require('../assertArraysEqual')
 
+/*
 console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // pass
 console.log(assertArraysEqual([1, 2, 3], [1, 2])) // fail
 console.log(assertArraysEqual([1, 2, 3], ["Hi", 2, 3])) // fail
+*/
+
+describe("#assertArraysEqualTest", () => {
+  it("returns 'Assertion Passed' for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2, 3]), "Assertion Passed");
+  });
+
+  it("returns 'Assertion Failed' for [1, 2, 3], [1, 2]", () => {
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [1, 2]), 'Assertion Failed');
+  })
+
+})
